4862ee01cb28be56e28a45ca52a8c60a4584c9b1
Parad0xial <64382080+Parad0xial@users.noreply.github.com>
2021-08-22 20:29:30 -0700

    more shtuff

--- Computer Chaos Game Jam/Assets/Scripts/OKScript.cs
+++ Computer Chaos Game Jam/Assets/Scripts/OKScript.cs
@@ -1,53 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class OKScript : MonoBehaviour
 {
 
-    public SpriteRenderer spriteRenderer;
     public Sprite pressedSprite;
     public Sprite unpressedSprite;
 
+    public Button okButton;
 
-    // Start is called before the first frame update
-    void Start()
+    void Start ()
     {
-        
-    }
+        Button btn = okButton.GetComponent<Button>();
+        btn.onClick.AddListener(taskOnClick);
+	}
 
-    // Update is called once per frame
-    void Update()
+    void taskOnClick()
     {
-
-        // Check for mouse input
-        if (Input.GetMouseButtonDown(0))
-         {
-        	Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-         	RaycastHit hit;
-        	// Casts the ray and get the first game object hit
-         	Physics.Raycast(ray, out hit);
-            ChangeSpritePressed();
-         	Debug.Log("This hit at " + hit.point );
-        }
-
-        if (Input.GetMouseButtonUp(0))
-        {
-            
-            ChangeSpriteUnpressed();
-
-		}
-    }
-
-    void ChangeSpritePressed()
-    {
-        spriteRenderer.sprite = pressedSprite; 
-    }
-
-    void ChangeSpriteUnpressed()
-    {
-        spriteRenderer.sprite = unpressedSprite; 
-    }
+        Debug.Log("fjdsklafjdsalk");
+	}
 }
 
     
