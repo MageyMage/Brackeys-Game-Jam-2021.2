77a2680013464721d1e7ef978a4d73b011fe1c99
Parad0xial <64382080+Parad0xial@users.noreply.github.com>
2021-08-22 19:07:59 -0700

    Decent Scene, Terrible Button Pressing

---
+++ Computer Chaos Game Jam/Assets/Scripts/DragScript.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragScript : MonoBehaviour
+{
+    
+    public GameObject window;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void OnMouseDown()
+    {
+        
+
+
+	}
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
