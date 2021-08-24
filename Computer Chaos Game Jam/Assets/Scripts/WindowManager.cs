25046cf5355a82da816dda29d5bf6e35d796aac2
Parad0xial <64382080+Parad0xial@users.noreply.github.com>
2021-08-23 21:51:14 -0700

    im genius it finally works

--- Computer Chaos Game Jam/Assets/Scripts/WindowManager.cs
+++ Computer Chaos Game Jam/Assets/Scripts/WindowManager.cs
@@ -1,33 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class WindowManager : MonoBehaviour
 {
     
     //script to handle spawing windows
 
+    public GameObject firstOKWindow;
     public GameObject okWindow;
     private Vector3 spawnPosition;
 
-    public Transform canvas;
+    private bool addListener = false;
+    private GameObject okWindowInstance;
 
+    private GameObject buttonGameObject;
+    private Button okButton;
 
     void Start()
     {
-        
+        buttonGameObject = firstOKWindow.transform.GetChild(0).gameObject;
+
+        okButton = buttonGameObject.GetComponent<Button>();
+        okButton.onClick.AddListener(SpawnOKWindow);
     }
 
 
     void Update()
     {
-        
+        if(addListener){
+            okButton = okWindowInstance.transform.GetChild(0).gameObject.GetComponent<Button>();
+            okButton.onClick.AddListener(SpawnOKWindow);
+
+            addListener = false;
+		}
     }
 
     public void SpawnOKWindow(){
         
+        Debug.Log("ooo");
         spawnPosition = new Vector3(0.0f, 0.0f, 0.0f);
-        GameObject instance = Instantiate(okWindow) as GameObject;
-        instance.transform.SetParent(GameObject.FindGameObjectWithTag("canvasy").transform, false);
+        okWindowInstance = Instantiate(okWindow) as GameObject;
+        okWindowInstance.transform.SetParent(GameObject.FindGameObjectWithTag("canvasy").transform, false);
+        
+        addListener = true;
 	}
 }
