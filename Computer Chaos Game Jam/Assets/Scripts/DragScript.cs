5953633988ef2273add6e891405272baa64afc45
Parad0xial <64382080+Parad0xial@users.noreply.github.com>
2021-08-23 18:45:50 -0700

    ok window finally works

--- Computer Chaos Game Jam/Assets/Scripts/DragScript.cs
+++ Computer Chaos Game Jam/Assets/Scripts/DragScript.cs
@@ -1,28 +1,12 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
-public class DragScript : MonoBehaviour {
+public class DragScript : MonoBehaviour, IPointerDownHandler {
 
-	private bool selected;
-
-	void Update(){
-	
-		if (selected == true){
-			Vector2 cursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-			transform.position = new Vector2(cursorPos.x, cursorPos.y);
-		}
-
-		if(Input.GetMouseButtonUp(0)){
-			selected = false;
-		}
-	}
-
-	void OnMouseOver(){
-	
-		if(Input.GetMouseButtonDown(0)){
-			selected = true;
-		}
-	}
+    public void OnPointerDown(PointerEventData eventData) {
+        Debug.Log("fdjsklaf");
+    }
 }
+
  
