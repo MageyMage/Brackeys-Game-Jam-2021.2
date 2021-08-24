f304511a625ccdc8476905af36059f2b81106e82
Parad0xial <64382080+Parad0xial@users.noreply.github.com>
2021-08-24 11:40:19 -0700

    messed around and didn't do much

--- Computer Chaos Game Jam/Assets/Scripts/WindowManager.cs
+++ Computer Chaos Game Jam/Assets/Scripts/WindowManager.cs
@@ -12,6 +12,8 @@ public class WindowManager : MonoBehaviour
     public GameObject okWindow;
     private Vector3 spawnPosition;
 
+    public BoxCollider2D windowArea;
+
     private bool addListener = false;
     private GameObject okWindowInstance;
 
@@ -29,21 +31,30 @@ public class WindowManager : MonoBehaviour
 
     void Update()
     {
-        if(addListener){
-            okButton = okWindowInstance.transform.GetChild(0).gameObject.GetComponent<Button>();
-            okButton.onClick.AddListener(SpawnOKWindow);
-
-            addListener = false;
-		}
+       
     }
 
     public void SpawnOKWindow(){
         
-        Debug.Log("ooo");
+        Debug.Log("Spawning Window");
         spawnPosition = new Vector3(0.0f, 0.0f, 0.0f);
-        okWindowInstance = Instantiate(okWindow) as GameObject;
+        okWindowInstance = Instantiate(okWindow, RandomPointInBounds(windowArea.bounds), Quaternion.identity) as GameObject;
         okWindowInstance.transform.SetParent(GameObject.FindGameObjectWithTag("canvasy").transform, false);
         
-        addListener = true;
+        addButtonListener(okWindowInstance);
 	}
+
+    
+    public void addButtonListener(GameObject okWindowInstance){
+        okButton = okWindowInstance.transform.GetChild(0).gameObject.GetComponent<Button>();
+        okButton.onClick.AddListener(SpawnOKWindow);
+	}
+
+    public static Vector3 RandomPointInBounds(Bounds bounds) {
+        return new Vector3(
+           Random.Range(bounds.min.x, bounds.max.x),
+           Random.Range(bounds.min.y, bounds.max.y),
+           0f
+        );
+    }
 }
