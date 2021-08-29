bc0927b78c2112599abcc22d314e35482c79909f
MageyMage <cole@redpeps.com>
2021-08-29 14:56:46 -0800

    Slider Looks better and windows spawn better

--- Computer Chaos Game Jam/Assets/Scripts/WindowMaker.cs
+++ Computer Chaos Game Jam/Assets/Scripts/WindowMaker.cs
@@ -20,7 +20,7 @@ public class WindowMaker : MonoBehaviour
             //spawns a button
             spawnPosition = RandomPointInBounds();
             GameObject okWindowInstance = Instantiate(okWindow, spawnPosition, Quaternion.identity) as GameObject;
-            okWindowInstance.transform.SetParent(GameObject.FindGameObjectWithTag("canvasy").transform, false);
+            okWindowInstance.transform.SetParent(GameObject.FindGameObjectWithTag("OKwindowParent").transform, false);
             okWindowInstance.transform.SetAsFirstSibling();
             i++;
         }
@@ -29,14 +29,14 @@ public class WindowMaker : MonoBehaviour
     }
 
 
-    //minimum x is -94 maximum x is 96
-    //maximum y is 45 minimum y is -37
+    //minimum x is -200 maximum x is 100
+    //maximum y is 100 minimum y is -85
     //idk why but the box colliders numbers are really low
     public static Vector3 RandomPointInBounds()
     {
         return new Vector3(
-           Random.Range(-94, 96),
-           Random.Range(-37, 45),
+           Random.Range(-153, 53),
+           Random.Range(-34, 50),
            0f
         );
     }
